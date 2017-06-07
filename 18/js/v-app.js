a114904704b810aed7a8720d283dcfb044b8a80b
Christian Medina <medinachristian1984@gmail.com>
2017-06-07 16:25:24 -0400

    Se modifica el ejercicio 18. Se utiliza el tag template dentro del HTML y se llama desde el componente por medio de su id

--- 18/js/v-app.js
+++ 18/js/v-app.js
@@ -1,17 +1,26 @@
+'use strict';
+
 Vue.component('app', {
-  template: `
-    <p>prueba</p>
-  `,
+  template: '#app-template',
   mounted() {
     axios.get('https://reqres.in/api/users?per_page=5')
-      .then(response => console.log(response));
+      .then(response => this.personas = response.data.data);
   },
   data() {
     return {
-
+      ganador: false,
+      personas: []
+    };
+  },
+  methods: {
+    elegirGanador() {
+      let cantidad = this.personas.length;
+      let indice = Math.floor((Math.random() * cantidad));
+      console.log(indice);
+      this.ganador = this.personas[indice];
     }
   }
-})
+});
 
 const vm = new Vue({
   el: 'main'
